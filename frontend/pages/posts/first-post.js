f15c5c8c326669291cad5ff75bda39239f7a16ba
Ray Li <raynli@outlook.com>
2023-02-07 19:00:12 -0800

    more info work

--- frontend/pages/posts/first-post.js
+++ frontend/pages/posts/first-post.js
@@ -1,9 +1,21 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+    <Image
+        src="/images/info.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Info"
+    />
+      <h1>About Classroom Finder</h1>
+        <p> We are ClassroomFinder, an app that provides directions to classrooms at UW.
+            To get directions from one UW building to another, you can type your start
+            and destination buildings in the 2 search bars. To get directions from a UW
+            building entrance to a specific room, you can type the building name in the first
+            search bar and the room number in the second.</p>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
