c98949079e34492161718c7a82dfaed51dbaaa0c
Kaiden Ong <kaiden.ong000@gmail.com>
2023-02-24 16:26:44 -0800

    create small graph w/ coordinates

---
+++ backend/shortest_path.js
@@ -0,0 +1,10 @@
+// aStar(graph, heuristic, start, goal) {
+
+// };
+
+function h(state) {
+    // let x = Math.abs(state.x - goal.x);
+    // let y = Math.abd(state.y - goal.y);
+    // let distance = Math.sqrt((x * x) + (y * y));
+    // return distance;
+}
