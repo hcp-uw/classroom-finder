5d36d2a252d0e822ffad72458c6f2707d75cd57f
HongjianYu <alvayhj@outlook.com>
2023-04-01 15:09:46 -0700

    Updates Priority Q

--- backend/src/building_item.js
+++ backend/src/building_item.js
@@ -1,6 +1,6 @@
 class building_item {
     constructor(building_tag, distance) {
-        this.building_tag = building_tag
-        this.distance = distance
+        this.building_tag = building_tag;
+        this.distance = distance;
     }
 }
--- backend/src/priority_queue.js
+++ backend/src/priority_queue.js
@@ -2,63 +2,64 @@
 class priority_queue {
 
     constructor() {
-        this.heap = []
+        this.heap = [];
     }
 
     add(building_tag, distance) {
-        this.heap.push(new building_item(building_tag, distance))
-        this.percolateUp(this.size() - 1)
+        this.heap.push(new building_item(building_tag, distance));
+        this.percolateUp(this.size() - 1);
     }
 
     peek() {
         if (this.isEmpty()) {
-            return null
+            return null;
         }
-        return this.heap[0]
+        return this.heap[0];
     }
 
     // Removes the node on top
     remove() {
         if (this.isEmpty()) {
-            return null
+            return null;
         }
-        let min = this.heap[0]
-        this.heap[0] = this.heap[this.size() - 1]
-        this.percolateDown(0)
-        return min
+        let min = this.heap[0];
+        this.heap[0] = this.heap[this.size() - 1];
+        this.heap.pop();
+        this.percolateDown(0);
+        return min;
     }
 
     clear() {
-        this.heap = []
+        this.heap = [];
     }
 
     isEmpty() {
-        return this.size() === 0
+        return this.size() === 0;
     }
 
     size() {
-        return this.heap.length
+        return this.heap.length;
     }
 
     // swim
     percolateUp(index) {
-        let parentIndex = Math.floor((index - 1) / 2)
+        let parentIndex = Math.floor((index - 1) / 2);
         while (parentIndex >= 0 && this.heap[index].distance < this.heap[index].distance) {
-            this.swap(index, parentIndex)
-            index = parentIndex
-            parentIndex = Math.floor((index - 1) / 2)
+            this.swap(index, parentIndex);
+            index = parentIndex;
+            parentIndex = Math.floor((index - 1) / 2);
         }
     }
 
     // sink
     percolateDown(index) {
-        let leftChildIndex = index * 2 + 1
-        let rightChildIndex = index * 2 + 2
+        let leftChildIndex = index * 2 + 1;
+        let rightChildIndex = index * 2 + 2;
         while (leftChildIndex <= this.size()) {
 
-            let smallerChildIndex = this.heap[leftChildIndex]
+            let smallerChildIndex = this.heap[leftChildIndex];
             if (rightChildIndex <= this.size() && this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
-                smallerChildIndex = this.heap[rightChildIndex]
+                smallerChildIndex = this.heap[rightChildIndex];
             }
 
             if (this.heap[index] < this.heap[smallerChildIndex]) {
@@ -67,14 +68,14 @@ class priority_queue {
                 this.swap(index, smallerChildIndex);
             }
             index = smallerChildIndex;
-            leftChildIndex = index * 2 + 1
-            rightChildIndex = index * 2 + 2
+            leftChildIndex = index * 2 + 1;
+            rightChildIndex = index * 2 + 2;
         }
     }
 
     swap(a, b) {
-        let x = this.heap[a]
-        this.heap[a] = this.heap[b]
-        this.heap[b] = x
+        let x = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = x;
     }
 }
