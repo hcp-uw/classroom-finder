4f82b7997869108260ac5b597064650269100472
Kaiden Ong <kaiden.ong000@gmail.com>
2023-04-19 16:56:50 -0700

    [+] Initial Dijkstra's Alg complete (missing edge weights)

---
+++ backend/a_star.js
@@ -0,0 +1,25 @@
+//import priority_queue from "priority_queue";
+
+class a_star {
+	a_star(problem) {
+		let initial_state = problem.create_intial_state; //write method in graph.js
+		let count = 0;
+		let max_open_length = 0;
+		let backlinks = {};
+		let closed = [];
+		backlinks[initial_state] = null;
+		let open = priority_queue();
+		open.enqueue(initial_state, 0); //write enqueue method in for pq
+		let g = {};
+		g[initial_state] = 0.0;
+
+		while (open.length > 0) {
+			// write length method for pq
+			if (open.length > max_open_length) {
+				max_open_length = open.length;
+			}
+		}
+	}
+}
+
+// Priority Queue: https://www.geeksforgeeks.org/implementation-priority-queue-javascript/
--- backend/dijkstras.js
+++ backend/dijkstras.js
@@ -1,64 +1,77 @@
-import graph from "graph.json";
+// import graph from "graph.json";
+import Graph from "./src/graph/index.ts";
+import PriorityQueue from "./src/priority_queue";
 
-// let adj = {};
-// adj["Bag"] = ["Bag (NE)", "Bgr", "Den"];
-// adj["Bag (NE)"] = ["Bag", "Cse", "Den"];
-// adj["Bgr"] = ["Bag", "Cs2", "Den"];
-// adj["Cse"] = ["Bag (NE)", "Cs2"];
-// adj["Cs2"] = ["Bgr", "Cse"];
-// adj["Den"] = ["Bag", "Bag (NE)", "Bgr"];
+let graph = new Graph();
+graph.addNode("Bag");
+graph.addNode("Bgr");
+graph.addNode("Cse");
 
-let graph = {};
-graph["Bag"] = {"Bag (NE)": 50, "Bgr": 60, "Den": 90};
-graph["Bag (NE)"] = {"Bag": 50, "Cse": 70, "Den": 80};
-graph["Bgr"] = {"Bag": 60, "Cs2": 100, "Den": 20};
-graph["Cse"] = {"Bag (NE)": 70, "Cs2": 150};
-graph["Cs2"] = {"Bgr": 100, "Cse": 150};
-graph["Den"] = {"Bag": 90, "Bag (NE)": 80, "Bgr": 20};
+graph.addEdge("Bag", "Bgr", "Path 1");
+graph.addEdge("Bag", "Cse", "Path 2");
+graph.addEdge("Cse", "Bgr", "Path 3");
+
+shortestPath = dijkstras(graph, "Bag", "Bgr");
+print(shortestPath);
+
+// graph["Bag"] = {"Bag (NE)": 50, "Bgr": 60, "Den": 90};
+// graph["Bag (NE)"] = {"Bag": 50, "Cse": 70, "Den": 80};
+// graph["Bgr"] = {"Bag": 60, "Cs2": 100, "Den": 20};
+// graph["Cse"] = {"Bag (NE)": 70, "Cs2": 150};
+// graph["Cs2"] = {"Bgr": 100, "Cse": 150};
+// graph["Den"] = {"Bag": 90, "Bag (NE)": 80, "Bgr": 20};
 
 function dijkstras(graph, startNode, endNode) {
+   let pq = new PriorityQueue();
    let distances = {};
-   let visited = [];
+   let path = [];
    let backtrace = {};
-   let pq = new priority_queue();
+   let smallest;
    pq.add(startNode, 0);
-   // this.nodes.forEach(node => {
-   for (let key in graph) {
-      if (key !== startNode) {
-         distances[node] = Infinity;
+   for (let vertex in graph.listNodes()) {
+      if (vertex === startNode) {
+         distances[vertex] = 0;
+         pq.add(vertex, 0);
+      } else {
+         distances[vertex] = Infinity;
+         pq.add(vertex, Infinity);
       }
-      backtrace[node] = null;
+      backtrace[vertex] = null;
    }
 
-   let currNode = startNode;
-   let weight = 0;
    while (!pq.isEmpty()) {
-      // let minNode = pq.remove();
-      // let currNode = minNode.data;
-      // let weight = minNode.priority;
-
-      for (let neighbor in graph[currNode]) {
-         if(!visited.includes(neighbor)) {
-            pq.add(neighbor);
+      smallest = pq.remove().val;
+      if (smallest === endNode.val) {
+         while(backtrace[smallest]) {
+            path.push(smallest);
+            smallest = backtrace[smallest];
          }
-         backtrace[neighbor.node] = currNode;
+         break;
+      } else { 
+         for (let neighbor in graph.listChildren(smallest)) {
+            let weight; // placeholder for edge weight
+            if (!(neighbor in backtrace)) {
+               pq.add(neighbor, weight);
+               distances[neighbor] = weight;
+               backtrace[neighbor] = smallest;
+            } else if (weight + distances[smallest] < distances[neighbor]) {
+               distances[neighbor] = weight + distances[smallest];
+               backtrace[neighbor] = smallest;
+            }
+         }
+      }
+
+      // Build shortest path
+      currNode = endNode;
+      while(currNode !== startNode) {
+         path.push(backtrace[currNode]);
+         currNode = backtrace[currNode];
       }
    }
-
-   priority_queue.add(getNext(node))
-   let node = closest(startNode)
+   return path;
 }
 
- function closest(node) {
-   for (let child in adj[node]) {
-      adj[child]
-   }
- }
-
- function getNext(node) {
-   for (let child in graph[node]) {
-
-   }
- }
-
- console.log(dijkstras(graph, "Bag", "Den"));
+//  References:
+//  https://www.freecodecamp.org/news/dijkstras-shortest-path-algorithm-visual-introduction/
+//  https://gist.github.com/Prottoy2938/66849e04b0bac459606059f5f9f3aa1a
+//  https://www.cs.usfca.edu/~galles/visualization/Dijkstra.html
